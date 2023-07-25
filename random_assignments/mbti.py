fee1a12ab034fa5464909d5f4a6dbc8757c56d7b
FavourPreciousOchonogor(Precy2020) <f.ochonogor@native.semicolon.africa>
2023-07-25 14:06:43 +0100

    add

--- random_assignments/mbti.py
+++ random_assignments/mbti.py
@@ -9,38 +9,101 @@ selection = [
     ["A. Plan, schedule ", "B. Unplanned, spontaneous"],
     ["A. seek many tasks, public activities, interaction with others ",
      "B. seek private, solitary activities with quiet to concentrate"],
-    {"A. standard, usual, conventional ", "B. different, novel, unique"},
-    {"A. firm, tend to criticize, hold the line ", "B. gentle, tend to appreciate, conciliate"},
-    {"A.regulated, structured ", "B. easygoing, live  and let live"},
-    {"A. external, communicative, express yourself ", "B. internal, reticent, keep to yourself"},
-    {"A. focus on here-and-now ", "B. look to the future, global perspective, \"big picture\""},
-    {"A. tough minded, just ", "B. tender-hearted, merciful"},
-    {"A. preparation, plan ahead ", "B. go with the flow, adapt as you go"},
-    {"A. active, initiate ", "B. reflective, deliberate"},
-    {"A. facts, things, \"what is\" ", "B. ideas, dreams, 'what could be', philosophical"},
-    {"A. matter of fact, issue oriented ", "B. sensitive, people-oriented, compassionate"},
-    {"A. control, govern ", "B. latitude, freedom"}]
+    ["A. standard, usual, conventional ", "B. different, novel, unique"],
+    ["A. firm, tend to criticize, hold the line ", "B. gentle, tend to appreciate, conciliate"],
+    ["A.regulated, structured ", "B. easygoing, live  and let live"],
+    ["A. external, communicative, express yourself ", "B. internal, reticent, keep to yourself"],
+    ["A. focus on here-and-now ", "B. look to the future, global perspective, \"big picture\""],
+    ["A. tough minded, just ", "B. tender-hearted, merciful"],
+    ["A. preparation, plan ahead ", "B. go with the flow, adapt as you go"],
+    ["A. active, initiate ", "B. reflective, deliberate"],
+    ["A. facts, things, \"what is\" ", "B. ideas, dreams, 'what could be', philosophical"],
+    ["A. matter of fact, issue oriented ", "B. sensitive, people-oriented, compassionate"],
+    ["A. control, govern ", "B. latitude, freedom"]]
 
-first_question = input(selection[0][0] + ' \t ' + selection[0][1])
-# if first_question != "A" or first_question != 'B':
-re
-second_question = input(selection[1][0] + ' \t ' + selection[1][1] + ' ')
-third_question = input(selection[2][0] + ' \t ' + selection[2][1])
-forth_question = input(selection[3][0] + ' \t ' + selection[3][1])
-fifth_question = input(selection[4][0] + ' \t ' + selection[4][1])
-sixth_question = input(selection[5][0] + ' \t ' + selection[5][1])
-seventh_question = input(selection[6][0] + ' \t ' + selection[6][1])
-eighth_question = input(selection[7][0] + ' \t ' + selection[7][1])
-ninth_question = input(selection[8][0] + ' \t ' + selection[8][1])
-tenth_question = input(selection[9][0] + ' \t ' + selection[9][1])
-eleventh_question = input(selection[10][0] + ' \t ' + selection[10][1])
-twelfth_question = input(selection[11][0] + ' \t ' + selection[11][1])
-thirteenth_question = input(selection[12][0] + ' \t ' + selection[12][1])
-fourteenth_question = input(selection[13][0] + ' \t ' + selection[13][1])
-fifteenth_question = input(selection[14][0] + ' \t ' + selection[14][1])
-sixteenth_question = input(selection[15][0] + ' \t ' + selection[15][1])
-seventeenth_question = input(selection[16][0] + ' \t ' + selection[16][1])
-eighteenth_question = input(selection[17][0] + ' \t ' + selection[17][1])
-nineteenth_question = input(selection[18][0] + ' \t ' + selection[18][1])
-twentieth_question = input(selection[19][0] + ' \t ' + selection[19][1])
 
+def first():
+    input(selection[0][0] + ' \t ' + selection[0][1])
+
+
+def second():
+    input(selection[1][0] + ' \t ' + selection[1][1] + ' ')
+
+
+def third():
+    input(selection[2][0] + ' \t ' + selection[2][1])
+
+
+def forth():
+    input(selection[3][0] + ' \t ' + selection[3][1])
+
+
+def fifth():
+    input(selection[4][0] + ' \t ' + selection[4][1])
+
+
+def sixth():
+    input(selection[5][0] + ' \t ' + selection[5][1])
+
+
+def seventh():
+    input(selection[6][0] + ' \t ' + selection[6][1])
+
+
+def eighth():
+    input(selection[7][0] + ' \t ' + selection[7][1])
+
+
+def ninth():
+    input(selection[8][0] + ' \t ' + selection[8][1])
+
+
+def tenth():
+    input(selection[9][0] + ' \t ' + selection[9][1])
+
+
+def eleventh():
+    input(selection[10][0] + ' \t ' + selection[10][1])
+
+
+def twelfth():
+    input(selection[11][0] + ' \t ' + selection[11][1])
+
+
+def thirteenth():
+    input(selection[12][0] + ' \t ' + selection[12][1])
+
+
+def fourteenth():
+    input(selection[13][0] + ' \t ' + selection[13][1])
+
+
+def fifteenth():
+    input(selection[14][0] + ' \t ' + selection[14][1])
+
+
+def sixteenth():
+    input(selection[15][0] + ' \t ' + selection[15][1])
+
+
+def seventeenth():
+    input(selection[16][0] + ' \t ' + selection[16][1])
+
+
+def eighteenth():
+    input(selection[17][0] + ' \t ' + selection[17][1])
+
+
+def nineteenth():
+    input(selection[18][0] + ' \t ' + selection[18][1])
+
+
+def twentieth():
+    input(selection[19][0] + ' \t ' + selection[19][1])
+
+
+first()
+if first() != 'A' or first() != 'B':
+    first()
+else:
+    second()
